ea87960505d51501d21a8eda80af7c1120b0796a
jtiearney <justin.tiearney@gmail.com>
2016-01-08 16:09:01 -0800

    OS Detection
    Find the current

--- pygui.py
+++ pygui.py
@@ -1,4 +1,5 @@
 import pyautogui
+import platform
 import time
 
 imageArray = ['play_hover.png','play.png', 'keep.png', 'keep_hover.png']
@@ -6,21 +7,33 @@ clicks = 0
 x = 0
 y = 0
 
+myPlatform = platform.system()
+print(pyautogui.size())
+
 def findButtons():
 	for i in imageArray:
-		foundButton = pyautogui.locateCenterOnScreen(i, region=(1350,1500,950,350))
+		foundButton = pyautogui.locateCenterOnScreen(i)#, region=(1350,1500,950,350))
 		if not foundButton is None: return (foundButton[0], foundButton[1])
 	return(x,y)
+
+def mouseClick(locX, locY):
+
+	if myPlatform == "Darwin":
+		pyautogui.click(locX/2, locY/2)
+	else:
+		pyautogui.click(locX, locY)
 
 buttons = findButtons()
 x, y = buttons
 while True:
-	if clicks > 20:
+	if clicks > 0:
 		buttons = findButtons()
 		x, y = buttons
 		clicks = 0
 	if x > 0 and y> 0:
-		pyautogui.click(x/2, y/2)
-		pyautogui.click(x/2, y/2)
+		mouseClick(x, y)
 		clicks = clicks + 1
+		print("Found it!")
+	else:
+		print("Not found")
 	time.sleep(2)
